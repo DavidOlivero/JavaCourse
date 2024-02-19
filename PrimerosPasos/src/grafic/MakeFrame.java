d2db570b13476b83950a09faea783405b0f07562
David Olivero <daniolidavid32@gmail.com>
2024-02-19 11:11:19 -0500

    UIX

--- PrimerosPasos/src/grafic/MakeFrame.java
+++ PrimerosPasos/src/grafic/MakeFrame.java
@@ -1,10 +1,29 @@
 package grafic;
 
+import java.awt.Frame;
+
+import javax.swing.*;
+
 public class MakeFrame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MyFrame frame = new MyFrame();
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
+}
+
+
+class MyFrame extends JFrame {
+	public MyFrame() {
+		// setSize(500, 300);
+		// setLocation(500, 300);
+		setBounds(500, 300, 550, 550);
+		// setResizable(true);
+		// setExtendedState(Frame.MAXIMIZED_BOTH);
+		
+		setTitle("My window");
+	}
 }
